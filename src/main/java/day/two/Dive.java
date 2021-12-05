65a0b73d4c11a25b1b257aa3cd2323e9167bd0f4
Joaquín Yeray Martín de la Nuez <joaquinmn@protonmail.com>
2021-12-05 23:21:55 +0000

    day2: refactor

--- src/main/java/day/two/Dive.java
+++ src/main/java/day/two/Dive.java
@@ -1,10 +1,16 @@
 package day.two;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public class Dive {
     private Integer horizontalPosition = 0;
     private int depth = 0;
+    private final Map<String, CommandExecutor> commandMap = Map.of(
+            "forward", moves -> horizontalPosition += moves,
+            "down", moves -> depth += moves,
+            "up", moves -> depth -= moves
+    );
 
     public int getHorizontalPosition() {
         return horizontalPosition;
@@ -15,18 +21,18 @@ public class Dive {
     }
 
     public void applyCurse(String course) {
-        Map<String, CommandExecutor> commandMap = Map.of(
-                "forward", moves -> horizontalPosition += moves,
-                "down", moves -> depth += moves,
-                "up", moves -> depth -= moves
-        );
-        String[] courseCommands = course.split(System.getProperty("line.separator"));
-        for (String courseCommand : courseCommands) {
-            String[] commandParameters = courseCommand.split(" ");
-            String commandOrder = commandParameters[0];
-            int moves = Integer.parseInt(commandParameters[1]);
-            commandMap.get(commandOrder).apply(moves);
-        }
+        Arrays.stream(getCommands(course)).forEach(this::apply);
+    }
+
+    private String[] getCommands(String course) {
+        return course.split(System.getProperty("line.separator"));
+    }
+
+    private void apply(String courseCommand) {
+        String[] commandParameters = courseCommand.split(" ");
+        String commandOrder = commandParameters[0];
+        int moves = Integer.parseInt(commandParameters[1]);
+        commandMap.get(commandOrder).apply(moves);
     }
 
     public int multiply() {
